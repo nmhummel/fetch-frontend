ed96ded37558ea045197a4faf40167ebe7988f0c
Natalie Hummel <69334473+nmhummel@users.noreply.github.com>
2022-11-29 21:12:04 -0600

    basic form built with handle functions

--- src/App.js
+++ src/App.js
@@ -43,56 +43,106 @@ export default function App() {
     getData()
   }, []);
 
-  console.log("dropdown data", allOccupations, allStates)
-  
+  const handleSubmit = (event) => {
+    // post to dataEndpoint
+    alert('Your submission has been submitted.');
+    event.preventDefault();
+  }
+
+  const handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
   return (
     <div className="App">
      <Header />
       <div className="py-1">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl text-fetchYellow font-bold lg:text-center">Join Fetch today and SAVE!</h1>
           <div className="text-2xl text-fetchYellow font-bold lg:text-center">
-          <h1>Join the Revolution!</h1>
             {loading && <div>A moment please...</div>}
             {error && (
               <div>{`There is a problem fetching the post data - ${error}`}</div>
             )}
-            <p>
-              Occupations:<br/> {allOccupations.map(occupation => {
-                return (
-                  <ul>
-                    <li>{occupation}</li>
-                  </ul>
-                )
-              })}
-            </p>
-            <br/>
-            <p>
-              States:<br/> {allStates.map(state => {
-                return (
-                  <ul>
-                    <li>{state.name}, {state.abbreviation}</li>
-                  </ul>
-                )
-              })}
-            </p>
-
           </div>
-
           <div className="mt-10">
-            <div className="mt-4 max-w-2xl h-30 rounded-2xl text-xl text-left w-2/3 text-white lg:mx-auto w-full p-4 bg-fetchYellow rounded-md shadow-card">
-
-
-              <p>Your task is to build a webpage with a user creation form. The form should take the following required inputs:</p>
-
-              <p>Full Name<br />Email<br/>Password<br/>Occupation<br/>State</p>
-              <p>Occupation and State should allow users to select from options returned by an endpoint. Users should only be able to select one occupation and one state. A GET request to <a href="https://frontend-take-home.fetchrewards.com/form">https://frontend-take-home.fetchrewards.com/form</a> will return a JSON body with the following format:</p>
-
-              <p>You should submit the results of the form to the same endpoint (https://frontend-take-home.fetchrewards.com/form) via a POST request with a JSON body of the following format:</p>
-
-              <p>The POST endpoint will return a 201 status code if all fields are provided. The response body will be the created user object. It does not perform any validation beyond this.</p>
+          {/* set the form section up on flex or grid to get the input fields all same width */}
+            <div className="mt-4 max-w-3xl h-30 rounded-2xl text-xl text-left w-2/3 text-black lg:mx-auto w-full p-4 bg-fetchYellow  rounded-md shadow-card">
+              <form onSubmit={handleSubmit}>
+                <p>
+                  <label>
+                    Full Name: <input type="text" name="name" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Email: <input type="text" name="email" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Password: <input type="text" name="password" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Select your occupation:
+                    <select value="Occupations:" onChange={handleChange} className="text-black rounded text-base m-2 pr-2 w-3/5">
+                      {allOccupations.map(occupation => {
+                        return(
+                          <option value={occupation}>{occupation}</option>
+                        )
+                      })}
+                    </select>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    State: 
+                    <select value="States:" onChange={handleChange} className="text-black rounded text-base m-2 pr-2 w-3/5">
+                      {allStates.map(data => {
+                        return(
+                          <option value={data.name}>{data.name} - {data.abbreviation}</option>
+                        )
+                      })}
+                    </select>
+                  </label>
+                </p>
+                <input type="submit" value="SUBMIT" className="bg-black border-white border-2 text-white rounded mt-3 text-xl px-8 py-2" />
+              </form>
             </div>
     
           </div>
+
+          {/* <div className="text-2xl text-fetchYellow font-bold lg:text-center">
+            <br/>
+              {loading && <div>A moment please...</div>}
+              {error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+              )} */}
+              {/* <p>
+                Occupations:<br/> {allOccupations.map(occupation => {
+                  return (
+                    <ul>
+                      <li>{occupation}</li>
+                    </ul>
+                  )
+                })}
+              </p>
+              <br/> */}
+              {/* <p>
+                States:<br/> {allStates.map(state => {
+                  return (
+                    <ul>
+                      <li>{state.name}, {state.abbreviation}</li>
+                    </ul>
+                  )
+                })}
+              </p> */}
+          {/* </div> */}
+
         </div>
       </div>
     </div>
