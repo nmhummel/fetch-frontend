b966195d1fd6c6b6d7738f76eba7a8ca89fec09e
Natalie Hummel <69334473+nmhummel@users.noreply.github.com>
2022-11-30 12:29:59 -0600

    restructured state data, 1st attempt at post

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Header from './Header';
 
 
 export default function App() {
-  // const [data, setData] = useState(null);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [occupation, setOccupation] = useState(null)
-  const [usaState, setUsaState] = useState(null)
   const [allOccupations, setAllOccupations] = useState([]);
   const [allStates, setAllStates] = useState([]);
-
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    occupation: "",
+    state: ""
+  });
+  
+  // console.log("signupData", signupData)
   const dataEndpoint = "https://frontend-take-home.fetchrewards.com/form";
 
   const getData = useCallback(()=>{
@@ -25,12 +31,10 @@ export default function App() {
       return response.json();
     })
     .then((actualData) => {
-        console.log(actualData)
         setAllStates(actualData.states);
         setAllOccupations(actualData.occupations);
     })
     .catch((err) => {
-        console.log(err.message);
         setError(err);
     })
     .finally(() => {
@@ -43,17 +47,46 @@ export default function App() {
     getData()
   }, []);
 
+  const postFormData = (signupData) => {
+    fetch(dataEndpoint, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({signupData})
+    })
+    .then(response => response.json()
+    .then(response => console.log("response", response)))
+  }
+  /// fix / finish this!
+  
+  const handleDataChange = (event) => {
+    console.log("e", event)
+    const { name, value, type } = event.target;
+    console.log('value', value)
+    console.log('name', name)
+    console.log('type', type)
+
+    setSignupData(previousData => {
+      return {
+        ...previousData,
+        [name]: value
+      }
+    });
+  };
+
   const handleSubmit = (event) => {
     // post to dataEndpoint
-    alert('Your submission has been submitted.');
     event.preventDefault();
+    postFormData(signupData)
+    alert('Your submission has been submitted.');
   }
 
-  const handleChange = (e) => {
-    this.setState({
-      value: e.target.value
-    });
-  }
+// validate email style
+// state and validate password requirements
+// create password hiding
+// alert styling 
 
   return (
     <div className="App">
@@ -67,81 +100,77 @@ export default function App() {
               <div>{`There is a problem fetching the post data - ${error}`}</div>
             )}
           </div>
-          <div className="mt-10">
-          {/* set the form section up on flex or grid to get the input fields all same width */}
-            <div className="mt-4 max-w-3xl h-30 rounded-2xl text-xl text-left w-2/3 text-black lg:mx-auto w-full p-4 bg-fetchYellow  rounded-md shadow-card">
-              <form onSubmit={handleSubmit}>
-                <p>
-                  <label>
-                    Full Name: <input type="text" name="name" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Email: <input type="text" name="email" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Password: <input type="text" name="password" className="text-black rounded text-base m-2 pr-2 w-4/5"/>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Select your occupation:
-                    <select value="Occupations:" onChange={handleChange} className="text-black rounded text-base m-2 pr-2 w-3/5">
-                      {allOccupations.map(occupation => {
-                        return(
-                          <option value={occupation}>{occupation}</option>
-                        )
-                      })}
-                    </select>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    State: 
-                    <select value="States:" onChange={handleChange} className="text-black rounded text-base m-2 pr-2 w-3/5">
-                      {allStates.map(data => {
-                        return(
-                          <option value={data.name}>{data.name} - {data.abbreviation}</option>
-                        )
-                      })}
-                    </select>
-                  </label>
-                </p>
-                <input type="submit" value="SUBMIT" className="bg-black border-white border-2 text-white rounded mt-3 text-xl px-8 py-2" />
+          <div className="m-10">
+            <div className="m-3 max-w-3xl h-30 rounded-2xl text-xl text-left w-1/2 text-black lg:mx-auto w-full p-4 bg-fetchYellow rounded-md shadow-card">
+              <form onSubmit={handleSubmit} className="place-content-center">
+                
+                <input className="text-black rounded text-base px-2 my-2 w-full"
+                  type="text" 
+                  placeholder="Full Name"
+                  onChange={handleDataChange}
+                  name="name" 
+                  value={signupData.name}
+                  required
+                />
+              
+                <input className="text-black rounded text-base px-2 my-2 w-full"
+                    type="text" 
+                    placeholder="Email Address"
+                    onChange={handleDataChange}
+                    name="email" 
+                    value={signupData.email}
+                    required
+                  />
+                
+                <input className="text-black rounded text-base px-2 my-2 w-full"
+                    type="text" 
+                    placeholder="Password"
+                    onChange={handleDataChange}
+                    name="password" 
+                    value={signupData.password}
+                    required
+                  />
+                
+                  <select className="text-black rounded text-base px-1 my-2 w-full"
+                    id="occupation"
+                    value={signupData.occupation}
+                    name="occupations" 
+                    onChange={handleDataChange} 
+                    required
+                  >
+                    <option value="">Select your occupation:</option>
+                    {allOccupations.map((occupation, index) => {
+                      return(
+                        <option key={index} value={occupation}>{occupation}</option>
+                      )
+                    })}
+                  </select>
+                
+                  <select className="text-black rounded text-base px-1 my-2 w-full"
+                    id="state"
+                    value={signupData.state} 
+                    name="states" 
+                    onChange={handleDataChange} 
+                    required
+                  >
+                    <option value="">Select your State"</option>
+                    {allStates.map((data, index) => {
+                      return(
+                        <option key={index} value={data.name}>{data.name} - {data.abbreviation}</option>
+                      )
+                    })}
+                  </select>
+                
+
+                <button className="bg-black border-white border-2 text-white w-full rounded mt-3 text-xl px-8 py-2"
+                  type="submit" 
+                >
+                  SUBMIT
+                </button>
               </form>
             </div>
     
           </div>
-
-          {/* <div className="text-2xl text-fetchYellow font-bold lg:text-center">
-            <br/>
-              {loading && <div>A moment please...</div>}
-              {error && (
-                <div>{`There is a problem fetching the post data - ${error}`}</div>
-              )} */}
-              {/* <p>
-                Occupations:<br/> {allOccupations.map(occupation => {
-                  return (
-                    <ul>
-                      <li>{occupation}</li>
-                    </ul>
-                  )
-                })}
-              </p>
-              <br/> */}
-              {/* <p>
-                States:<br/> {allStates.map(state => {
-                  return (
-                    <ul>
-                      <li>{state.name}, {state.abbreviation}</li>
-                    </ul>
-                  )
-                })}
-              </p> */}
-          {/* </div> */}
 
         </div>
       </div>
